8bc9d8b9ab64b11b31d5219e8c3e94f52a495994
PurpleMyst <PurpleMyst@users.noreply.github.com>
2025-12-04 02:12:21 +0100

    perf(day02): rayon + no string conversion

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -1,22 +1,32 @@
-use std::{fmt::Display, iter::zip};
+use std::fmt::Display;
+
+use rayon::prelude::*;
 
 fn is_valid_id_for_p1(n: u64) -> bool {
-    let s = n.to_string();
-    if s.len() % 2 != 0 {
+    let l = n.ilog10()  + 1;
+    if l % 2 != 0 {
         return true;
     }
-    let l = s.len() / 2;
-    s.as_bytes()[..l] != s.as_bytes()[l..]
+    n % 10u64.pow(l/2 as u32)
+    != n / 10u64.pow(l/2 as u32)
 }
 
 fn is_valid_id_for_p2(n: u64) -> bool {
-    let s = n.to_string();
-    for l in 1..=s.len() / 2 {
-        let mut chunks = s.as_bytes().chunks(l);
-        let representative = chunks.next().unwrap();
-        if chunks.all(|c| c == representative) {
-            return false;
+    let l = n.ilog10() + 1;
+    'outer: for k in 1..=l / 2 {
+        if l % k != 0 {
+            continue;
         }
+        let mask = 10u64.pow(k as u32);
+        let first_part = n % mask;
+        let mut rest = n / mask;
+        while rest != 0 {
+            if rest % mask != first_part {
+                continue 'outer;
+            }
+            rest /= mask;
+        }
+        return false;
     }
     true
 }
@@ -25,29 +35,32 @@ fn is_valid_id_for_p2(n: u64) -> bool {
 pub fn solve() -> (impl Display, impl Display) {
     let input = include_str!("input.txt");
 
-    let part1 = input
-        .trim()
-        .split(',')
-        .map(|line| {
-            let (start, end) = line.split_once('-').unwrap();
-            let start = start.parse::<u64>().unwrap();
-            let end = end.parse::<u64>().unwrap();
-            (start..=end).filter(|&n| !is_valid_id_for_p1(n)).sum::<u64>()
-        })
-        .sum::<u64>();
-
-    let part2 = input
-        .trim()
-        .split(',')
-        .map(|line| {
-            let (start, end) = line.split_once('-').unwrap();
-            let start = start.parse::<u64>().unwrap();
-            let end = end.parse::<u64>().unwrap();
-            (start..=end).filter(|&n| !is_valid_id_for_p2(n)).sum::<u64>()
-        })
-        .sum::<u64>();
-
-    (part1, part2)
+    rayon::join(
+        || {
+            input
+                .trim()
+                .par_split(',')
+                .map(|line| {
+                    let (start, end) = line.split_once('-').unwrap();
+                    let start = start.parse::<u64>().unwrap();
+                    let end = end.parse::<u64>().unwrap();
+                    (start..=end).filter(|&n| !is_valid_id_for_p1(n)).sum::<u64>()
+                })
+                .sum::<u64>()
+        },
+        || {
+            input
+                .trim()
+                .par_split(',')
+                .map(|line| {
+                    let (start, end) = line.split_once('-').unwrap();
+                    let start = start.parse::<u64>().unwrap();
+                    let end = end.parse::<u64>().unwrap();
+                    (start..=end).filter(|&n| !is_valid_id_for_p2(n)).sum::<u64>()
+                })
+                .sum::<u64>()
+        },
+    )
 }
 
 #[cfg(test)]
