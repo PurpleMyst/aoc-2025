1c167e57a2e9355594c83eb939d67a6209040f0d
PurpleMyst <PurpleMyst@users.noreply.github.com>
2025-12-07 06:21:53 +0100

    perf(day07): memoize via grid

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,13 +1,10 @@
-use std::{collections::HashSet, fmt::Display, mem::swap};
+use std::{fmt::Display, mem::swap};
 
 use grid::Grid;
-use memoize::memoize;
-use rustc_hash::FxHashMap;
+use rustc_hash::FxHashSet;
 
 #[inline]
 pub fn solve() -> (impl Display, impl Display) {
-    memoized_flush_solve_part2(); // for benchmarking purposes
-
     let input = include_str!("input.txt");
     let width = input.lines().next().unwrap().len();
     let map = Grid::from_vec(input.lines().flat_map(|l| l.bytes()).collect(), width);
@@ -17,8 +14,8 @@ pub fn solve() -> (impl Display, impl Display) {
 }
 
 fn solve_part1(map: &Grid<u8>, start: (usize, usize)) -> i32 {
-    let mut beams = HashSet::new();
-    let mut new_beams = HashSet::new();
+    let mut beams = FxHashSet::default();
+    let mut new_beams = FxHashSet::default();
     beams.insert(start);
     let mut part1 = 0;
     while !beams.is_empty() {
@@ -39,27 +36,44 @@ fn solve_part1(map: &Grid<u8>, start: (usize, usize)) -> i32 {
                 new_beams.insert((new_y, x));
             }
         }
-
         swap(&mut beams, &mut new_beams);
     }
     part1
 }
 
-#[memoize(Ignore: map, CustomHasher: FxHashMap, HasherInit: FxHashMap::default())]
 fn solve_part2(map: &Grid<u8>, pos: (usize, usize)) -> usize {
+    let mut memo: Grid<Option<usize>> = Grid::new(map.rows(), map.cols());
+    solve_part2_rec(map, pos, &mut memo)
+}
+
+fn solve_part2_rec(map: &Grid<u8>, pos: (usize, usize), memo: &mut Grid<Option<usize>>) -> usize {
+    if let Some(cached_result) = memo[pos] {
+        return cached_result;
+    }
+
     let (y, x) = pos;
     let new_y = y + 1;
-    if new_y == map.rows() {
-        return 1;
-    }
-    if map[(new_y, x)] == b'^' {
-        (if x != 0 { solve_part2(map, (y, x - 1)) } else { 0 })
-            + (if x != map.cols() - 1 {
-                solve_part2(map, (y, x + 1))
-            } else {
-                0
-            })
+
+    let result = if new_y == map.rows() {
+        1
+    } else if map[(new_y, x)] == b'^' {
+        let left = if x != 0 {
+            solve_part2_rec(map, (y, x - 1), memo)
+        } else {
+            0
+        };
+
+        let right = if x != map.cols() - 1 {
+            solve_part2_rec(map, (y, x + 1), memo)
+        } else {
+            0
+        };
+
+        left + right
     } else {
-        solve_part2(map, (new_y, x))
-    }
+        solve_part2_rec(map, (new_y, x), memo)
+    };
+
+    memo[pos] = Some(result);
+    result
 }
